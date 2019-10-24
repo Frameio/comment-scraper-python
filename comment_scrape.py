00115a72c020a59bda2c4e5c8a6df0942dfb6421
Erikka <esinnes42@yahoo.com>
2019-10-23 18:48:01 -0700

    removed print statement

--- comment_scrape.py
+++ comment_scrape.py
@@ -1,19 +1,13 @@
 # This sample shows you how scrape comments from a project.
 # Pass the root_asset_id and a developer token in to get_all_project_comments
-# to scrape the comments from a project. This sample does not include
-# version stacks.
+# to scrape the comments from a project. 
 
 from frameioclient import FrameioClient
 from flask import Flask, jsonify, request
-import requests, json, pprint, csv, itertools
+import requests, json, csv, itertools
 
 ROOT_ASSET_ID = "2cb29192-025b-4c11-b908-9b4331baa67a"
 TOKEN = "fio-u-m6BqazQLLTgKc6fatHZ-gncudL7-3WUZhZ4a7W_RfV7cd9waZ8tNhOhFd1wZ-RVF"
-# Function for grabbing comments from assets. It's used by
-# get_all_project_comments. It takes an initialized client,
-# asset_id, and reference to an empty list. It returns
-# all comments on all assets. This sample does not include
-# version stacks.
 
 def all_comments(client, asset_id, comment_list):
     files = client.get_asset_children(asset_id)
@@ -55,7 +49,6 @@ response_lists = [r.results for r in responses]
 # Flatten out response_lists so that there's only one item in each part of the list
 flat_response_list = list(itertools.chain.from_iterable(response_lists))
 
-pprint.pprint(flat_response_list)
 # Now we can use list comprehension to grab what we want from each block in the list and make a new flat list.
 list_for_csv = [[o['text'], o['parent_id'], o['asset_id'], o['owner_id'], o['owner']['email'], o['timestamp'], o['updated_at']] for o in flat_response_list]
 
